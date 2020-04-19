99ff188cb28272ce2d35c4ce5c066a1dbffe4b60
nanBole <443626775@qq.com>
2020-04-19 15:39:57 +0800

    main文件提交

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,5 +1,10 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("你好");
+
+        int x = 2;
+        int y =3;
+        System.out.println(x*y);
     }
+
 }
